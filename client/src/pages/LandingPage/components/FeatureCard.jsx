4d27626e7ffd037de3631180ccc955d2f750a126
SUMIT SINGH <sumitsingh@SUMITs-MacBook-Air-2.local>
2023-11-01 00:15:30 +0530

    feature card initiated

---
+++ client/src/pages/LandingPage/components/FeatureCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./FeatureCard.module.css"
+
+const FeatureCard = () => {
+  return (
+    <div className={styles.Card}>
+      <div className={styles.featureImg}>
+        <img src="" alt="this is an img" />
+      </div>
+      <h3>Share Notes with Friends</h3>
+      
+    </div>
+  )
+}
+
+export default FeatureCard
