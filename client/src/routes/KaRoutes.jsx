8aaeafbdd108466fc42ededbe0f1e6ab92e3b027
SUMIT SINGH <sumitsingh@SUMITs-MacBook-Air-2.local>
2023-10-31 22:49:44 +0530

    Landing page initiated

--- client/src/routes/KaRoutes.jsx
+++ client/src/routes/KaRoutes.jsx
@@ -7,6 +7,7 @@ import Navbar from "../components/Navbar/Navbar";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import CallerPage from "../pages/CallerPage/CallerPage";
+import LandingPage from "../pages/LandingPage/LandingPage";
 
 function KaRoutes() {
   const { userToken } = useSelector((state) => state.loginReducer);
@@ -20,7 +21,8 @@ function KaRoutes() {
       <Navbar />
       <div>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/chat" element={<ChatPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/call/:user" element={<CallerPage />} />
