22af86587b77bf866700692c03691258109088ec
raman kumar <ramankrchoudhary11@gmail.com>
2023-11-10 21:55:35 +0530

    removing console

--- server/midddleware/error.js
+++ server/midddleware/error.js
@@ -1,5 +1,4 @@
 const handleError = (err, req, res, next) => {
-  console.error(err);
   err.statusCode = err.statusCode || 500;
 
   return res.status(err.statusCode).send({ success: false, msg: err.message });
